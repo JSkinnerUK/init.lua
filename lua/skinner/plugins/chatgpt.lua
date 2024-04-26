8b2f878e797cab6579af5ecf0ddc5991339487de
James Skinner <56730344+JSkinnerUK@users.noreply.github.com>
2024-04-26 15:28:07 +0100

    ChatGPT keybind

--- lua/skinner/plugins/chatgpt.lua
+++ lua/skinner/plugins/chatgpt.lua
@@ -2,3 +2,7 @@ local home = vim.fn.expand("$HOME")
 require("chatgpt").setup({
     api_key_cmd = "gpg --decrypt " .. home .. "/secrets/secret.gpg"
 })
+
+vim.keymap.set("n", "<leader>ch", ':lua vim.cmd(":ChatGPT")<CR>',
+  {silent = true, noremap = true}
+)
